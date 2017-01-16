884ccfc2669b3f41b556b61036ae2f17f7467aeb
Ari <amindell11@gmail.com>
2017-01-15 23:13:19 -0500

    added unit test for kenneth

---
+++ Simulation-Ohm/src/simulation/UnitTest.java
@@ -0,0 +1,20 @@
+package simulation;
+
+import java.util.List;
+
+import org.newdawn.slick.geom.Polygon;
+import org.newdawn.slick.geom.Shape;
+
+import com.team1389.util.AddList;
+
+public class UnitTest {
+	public static void main(String[] args) {
+		// writeShapes(getShapeList(),getShapeList(),getShapeList());
+	}
+
+	public List<Shape> getShapeList() {
+		AddList<Shape> s = new AddList<>();
+		return s.put(new Polygon(new float[] { 0, 0, 0, 1, 1, 1, 1, 0 }), new Polygon(new float[] { 0, 0, 0, 1, 1, 1, 1, 0 }),
+				new Polygon(new float[] { 0, 0, 0, 1, 1, 1, 1, 0 }));
+	}
+}
