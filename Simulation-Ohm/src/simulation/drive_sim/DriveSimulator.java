7ede362b2c451075a68a3c07b3e7be7169dc4f46
RaphaelMetz <raphaeljmetz@gmail.com>
2017-02-08 17:07:45 -0500

    junk

--- Simulation-Ohm/src/simulation/drive_sim/DriveSimulator.java
+++ Simulation-Ohm/src/simulation/drive_sim/DriveSimulator.java
@@ -23,6 +23,7 @@ import net.java.games.input.Component.Identifier.Key;
 import simulation.Simulator;
 import simulation.drive_sim.auto_sim.AutoOptionTwo;
 import simulation.drive_sim.auto_sim.AutoOptionOne;
+import simulation.drive_sim.auto_sim.AutoOptionThree;
 import simulation.drive_sim.field.SimulationField;
 import simulation.drive_sim.robot.OctoRobot;
 import simulation.drive_sim.robot.RenderableRobot;
@@ -122,7 +123,7 @@ public class DriveSimulator extends BasicGame {
 	public void init(GameContainer arg0) throws SlickException {
 		field = new SimulationField(width, height);
 		robot = new OctoRobot(field, Alliance.RED);
-		workbench = new AutoOptionTwo(robot);
+		workbench = new AutoOptionThree(robot);
 
 		KeyboardHardware hardware = new KeyboardHardware();
 		controlZ = hardware.getKey(Key.LCONTROL).combineAND(hardware.getKey(Key.Z)).getLatched();
