6c29dbd1264ba5d5fe5fd1532fed9f6a8ec13659
Ari Mindell <amindell11@gmail.com>
2017-03-28 22:22:29 -0400

    use pair for mapping

--- Simulation-Ohm/src/simulation/input/MappedController.java
+++ Simulation-Ohm/src/simulation/input/MappedController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import com.team1389.util.Pair;
+
 import net.java.games.input.Component;
 import net.java.games.input.Component.Identifier;
 import net.java.games.input.Controller;
@@ -26,7 +28,7 @@ public class MappedController {
 	public double getAxis(int axis) {
 		poll();
 		if (axis < axes.size()) {
-			return axes.get(axis).y.getPollData();
+			return axes.get(axis).component().getPollData();
 		}
 		return 0;
 	}
@@ -34,26 +36,40 @@ public class MappedController {
 	public boolean getButton(int button) {
 		poll();
 		if (button < buttons.size()) {
-			return buttons.get(button).y.getPollData() > 0;
+			return buttons.get(button).component().getPollData() > 0;
 		}
 		return false;
 	}
 
 	public static MappedController mapController(Controller c) {
 		Component[] axes = Arrays.stream(c.getComponents()).filter(r -> r.isAnalog()).toArray(Component[]::new);
-		List<Mapping> mappedAxes = IntStream.range(0, axes.length).mapToObj(i -> new Mapping(i, axes[i]))
-				.collect(Collectors.toList());
-		Component[] btns = Arrays.stream(c.getComponents())
-				.filter(r -> !(r.isAnalog() || r.getIdentifier() == Identifier.Axis.POV)).toArray(Component[]::new);
-		List<Mapping> mappedBtns = IntStream.range(0, btns.length).mapToObj(i -> new Mapping(i, btns[i]))
-				.collect(Collectors.toList());
+		List<Mapping> mappedAxes = IntStream
+				.range(0, axes.length)
+					.mapToObj(i -> new Mapping(i, axes[i]))
+					.collect(Collectors.toList());
+		Component[] btns = Arrays
+				.stream(c.getComponents())
+					.filter(r -> !(r.isAnalog() || r.getIdentifier() == Identifier.Axis.POV))
+					.toArray(Component[]::new);
+		List<Mapping> mappedBtns = IntStream
+				.range(0, btns.length)
+					.mapToObj(i -> new Mapping(i, btns[i]))
+					.collect(Collectors.toList());
 		return new MappedController(c, mappedAxes, mappedBtns);
 
 	}
 
-	static class Mapping extends Tuple<Integer, Component> {
+	static class Mapping extends Pair<Integer, Component> {
 		Mapping(Integer x, Component y) {
 			super(x, y);
+		}
+
+		public Integer port() {
+			return one;
+		}
+
+		public Component component() {
+			return two;
 		}
 
 	}
